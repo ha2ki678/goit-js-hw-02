e1898cbf4978f1ee59576420db1a26e2426cb4a4
ha2ki678 <caskona228678@gmail.com>
2026-07-12 19:47:59 +0200

    hw-02
    task-04

--- js/task-4.js
+++ js/task-4.js
@@ -14,8 +14,10 @@ function getShippingCost(country) {
       price = 120;
       break;
     default:
-      return "Sorry, there is no delivery to your country";}
-  return `Shipping to ${country} will cost ${price} credits`;}
+      return "Sorry, there is no delivery to your country";
+  }
+  return `Shipping to ${country} will cost ${price} credits`;
+}
 console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
 console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
