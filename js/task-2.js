59deed2c24372eca7562e9508c8234ad9079d5bd
ha2ki678 <caskona228678@gmail.com>
2026-07-12 19:37:37 +0200

    hw-02
    task-02

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,9 @@
 function formatMessage(message, maxLength) {
   if (message.length <= maxLength) {
-    return message;}
-  return message.slice(0, maxLength) + "...";}
+    return message;
+  }
+  return message.slice(0, maxLength) + "...";
+}
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
 console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
 console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
