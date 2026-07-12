1581941a90c608e8f5567413a3ed9ff2311e6bad
ha2ki678 <caskona228678@gmail.com>
2026-07-12 19:47:06 +0200

    hw-02
    task-03

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,7 @@
 function checkForSpam(message) {
   const newMessage = message.toLowerCase();
-  return newMessage.includes("spam") || newMessage.includes("sale");}
+  return newMessage.includes("spam") || newMessage.includes("sale");
+}
 console.log(checkForSpam("Latest technology news")); // false
 console.log(checkForSpam("JavaScript weekly newsletter")); // false
 console.log(checkForSpam("Get best sale offers now!")); // true
